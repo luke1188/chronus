287977651da1fb4d01506203e92dfdfb6b09b038
luke1188 <49883653+luke1188@users.noreply.github.com>
2023-09-05 23:02:29 -0400

    css work, current-time

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -9,15 +9,18 @@ import { format, addDays, startOfWeek, endOfWeek, startOfMonth, endOfMonth, getD
 })
 export class SchedulerComponent implements OnInit {
   currentDate: string = '';
+  currentTime: Date = new Date();
   schedule: Date[] = [];
   weekDays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // Names of days of the week
   timeSlots: string[] = [
-    '12a', '1a', '2a', '3a', '4a', '5a', '6a',
-    '7a', '8a', '9a', '10a', '11a', '12p',
-    '1p', '2p', '3p', '4p', '5p', '6p',
-    '7p', '8p', '9p', '10p', '11p',
-    // Add more time slots as needed
-  ];
+  '12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM', '3:00 AM', '3:30 AM',
+  '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM', '7:30 AM',
+  '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM',
+  '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM',
+  '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM',
+  '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM',
+];
+
   currentView: 'day' | 'week' | 'month' = 'month'; // Default view
   weeks: Date[][] = [];
   selectedMonth: number = new Date().getMonth(); // Default to current month
@@ -29,9 +32,17 @@ export class SchedulerComponent implements OnInit {
 
   constructor() { }
 
+  updateCurrentTime() {
+    this.currentTime = new Date();
+  }
+
   ngOnInit(): void {
     this.currentDate = this.formatDay(new Date()); // Initialize with the current date
     this.generateSchedule();
+    // Update the current time periodically (e.g., every minute)
+    setInterval(() => {
+      this.updateCurrentTime();
+    }, 60000); // Update every minute (adjust as needed)
   }
 
   generateSchedule(): void {
@@ -95,4 +106,14 @@ export class SchedulerComponent implements OnInit {
     // Update the displayed month when the user changes it
     this.generateSchedule();
   }
+  isCurrentTimeSlot(timeSlot: string): boolean {
+    const currentTime = new Date();
+    const [hourMinute, period] = timeSlot.split(' ');
+    const [currentHour, currentMinute] = currentTime.toLocaleTimeString().split(':');
+    const currentPeriod = currentTime.getHours() < 12 ? 'AM' : 'PM';
+
+    return currentPeriod === period && currentHour === hourMinute.split(':')[0] && currentMinute >= '00' && currentMinute <= '29';
+  }
+
+
 }
